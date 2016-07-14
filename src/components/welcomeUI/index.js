e3dde51a42c2455875a05ecb1507e89d23b206f0
iQoderi <841599872@qq.com>
2016-07-14 21:14:00 +0800

    加入了对back监听的功能，以及2秒内按两次back键退出应用

--- src/components/welcomeUI/index.js
+++ src/components/welcomeUI/index.js
@@ -8,7 +8,9 @@ import {
     Text,
     Navigator,
     Image,
-    Alert
+    Alert,
+    BackAndroid,
+    ToastAndroid
 }from 'react-native';
 
 import  CommonStyles from '../../style/index.style';
@@ -17,21 +19,47 @@ import SideMenu from '../../containers/sideMenu.container';
 
 class WelCome extends Component {
     // 构造
-      constructor(props) {
+    constructor(props) {
         super(props);
         // 初始状态
-          this.timer=null;
-      }
+        this.timer = null;
+        this._onBackAndroid = this._onBackAndroid.bind(this);
+    }
+
+    _onBackAndroid() {
+        const {navigator}=this.props;
+        let routeLen = navigator.getCurrentRoutes().length;
+        if (routeLen > 1) {
+            navigator.pop();
+            return true;
+        } else {
+            if (this.lastBackPress && this.lastBackPress + 2000 >= Date.now()) {
+                return false;
+            }
+            this.lastBackPress = Date.now();
+            ToastAndroid.show('再按一次退出应用', ToastAndroid.SHORT);
+            return true;
+        }
+    }
+    
+    componentWillMount() {
+        BackAndroid.addEventListener('hardwareBackPress', this._onBackAndroid);
+    }
+
+    componentWillUnMount() {
+        BackAndroid.removeEventListener('hardwareBackPress', this._onBackAndroid);
+    }
+
     componentDidMount() {
-        const TIME=1500;
+        const TIME = 1500;
         const {navigator} =this.props;
         this.timer = setTimeout(()=> {
             navigator.replace({
-                name:'home',
-                component:SideMenu
+                name: 'home',
+                component: SideMenu
             });
             clearTimeout(this.timer);
-        },TIME)
+        }, TIME)
     }
 
     render() {
